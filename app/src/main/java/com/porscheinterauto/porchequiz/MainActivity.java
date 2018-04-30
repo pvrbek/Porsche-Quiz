1d825f211a504476b807f58234ea295f992f3b0c
pvrbek <petar.vrbek@gmail.com>
2018-04-30 15:27:45 +0200

    Dorađen menu - sada radi otvaranje i zatvaranje menu-a i na pritisak

--- app/src/main/java/com/porscheinterauto/porchequiz/MainActivity.java
+++ app/src/main/java/com/porscheinterauto/porchequiz/MainActivity.java
@@ -1,24 +1,36 @@
 package com.porscheinterauto.porchequiz;
 
+
 import android.content.DialogInterface;
 import android.content.Intent;
+import android.os.Bundle;
+import android.support.annotation.NonNull;
+import android.support.design.widget.NavigationView;
+import android.support.v4.app.Fragment;
+import android.support.v4.view.GravityCompat;
+import android.support.v4.widget.DrawerLayout;
+import android.support.v7.app.ActionBar;
+import android.support.v7.app.ActionBarDrawerToggle;
 import android.support.v7.app.AlertDialog;
 import android.support.v7.app.AppCompatActivity;
-import android.os.Bundle;
+import android.support.v7.widget.Toolbar;
+import android.view.Menu;
+import android.view.MenuItem;
 import android.view.View;
 import android.widget.Button;
 import android.widget.TextView;
-import android.widget.Toolbar;
 
 import java.util.Random;
 
 //Video: Develop simple Quiz in Android Studio, Vrijeme: 6:00
 public class MainActivity extends AppCompatActivity {
 
-    Button answer2, answer3, answer4;
+    Button answer2, answer3, answer4,button1;
     TextView score, question;
     private Questions mQuestions = new Questions();
     private String mAnswer;
+    private DrawerLayout mDrawerLayout;
+    private ActionBarDrawerToggle mToggle;
     private int mScore=0;
     private int mQuestionLength = mQuestions.mQuestions.length;
     public int counter=0;
@@ -26,9 +38,74 @@ public class MainActivity extends AppCompatActivity {
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.nav_drawer);
+        setContentView(R.layout.home);
+        setToolbar();
+        configureNavigationDrawer();
+        button1 = findViewById(R.id.button1);
+        button1.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Intent intentNav= new Intent(MainActivity.this,NewActivity.class);
+                startActivity(intentNav);
+            }
+        });
 
     }
+
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        getMenuInflater().inflate(R.menu.empty, menu);
+        return true;
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+        int itemId = item.getItemId();
+        switch (itemId){
+            case android.R.id.home:
+                mDrawerLayout.openDrawer(GravityCompat.START);
+                return true;
+        }
+
+        return super.onOptionsItemSelected(item);
+    }
+
+    private void setToolbar(){
+        Toolbar tb = findViewById(R.id.tool_bar);
+        setSupportActionBar(tb);
+        ActionBar ab = getSupportActionBar();
+        ab.setDisplayHomeAsUpEnabled(true);
+        ab.setHomeAsUpIndicator(R.drawable.ic_action_menu);
+    }
+
+    private void configureNavigationDrawer(){
+        mDrawerLayout = findViewById(R.id.mainLayout);
+        NavigationView navigationView = findViewById(R.id.navigation);
+        navigationView.setNavigationItemSelectedListener(new NavigationView.OnNavigationItemSelectedListener() {
+            @Override
+            public boolean onNavigationItemSelected(@NonNull MenuItem item) {
+                Fragment f = null;
+                int itemId = item.getItemId();
+
+                if (itemId == R.id.refresh){
+                    f = new RefreshFragment();
+                } else if (itemId == R.id.stop){
+                    f = new StopFragment();
+                }
+                if (f != null){
+                    android.support.v4.app.FragmentTransaction transaction = getSupportFragmentManager().beginTransaction();
+                    transaction.replace(R.id.frame,f);
+                    transaction.commit();
+                    mDrawerLayout.closeDrawers();
+                    return true;
+
+                }
+
+                return false;
+            }
+        });
+    }
+
     private void updateQuestion(int num){
         question.setText(mQuestions.getQuestion(num));
         answer2.setText(mQuestions.getChoice2(num));
---
+++ app/src/main/java/com/porscheinterauto/porchequiz/RefreshFragment.java
@@ -0,0 +1,18 @@
+package com.porscheinterauto.porchequiz;
+
+import android.annotation.SuppressLint;
+import android.support.v4.app.Fragment;
+import android.os.Bundle;
+import android.support.annotation.Nullable;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+@SuppressLint("ValidFragment")
+public class RefreshFragment extends Fragment{
+    @Nullable
+    @Override
+    public View onCreateView(LayoutInflater inflater, @Nullable ViewGroup container, Bundle savedInstanceState) {
+        View v = inflater.inflate(R.layout.activity_main,container, false);
+        return v;
+    }
+}
